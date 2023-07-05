43a20a1ab3218e5f1534adabccb16fd9b219eca6
DerekAyala <derek.ayla12345@hotmail.com>
2023-07-05 11:48:19 -0600

    Add environment variables to .env file and tested
    - Added the necessary environment variables to the .env file, such as API keys, database connection details, or other configuration settings.
    - Made sure that the .env file is securely stored and not committed to version control systems.
    - Updated the project's configuration files to load the environment variables from the .env file using react-native-dotenv.

--- AppVuelos/firebase.ts
+++ AppVuelos/firebase.ts
@@ -1,14 +1,23 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_UR,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGINGSENDER_ID,
+  APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCMBjX8Dm0-hVCTEe0OeEI5V1ZkQuuCIew',
-  authDomain: 'appvuelos-16ac9.firebaseapp.com',
-  databaseURL: 'https://appvuelos-16ac9-default-rtdb.firebaseio.com/',
-  projectId: 'appvuelos-16ac9',
-  storageBucket: 'gs://appvuelos-16ac9.appspot.com',
-  messagingSenderId: '227704013235',
-  appId: '1:227704013235:android:ca6cb9c44cbbdd9b6dcbc6',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_UR,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGINGSENDER_ID,
+  appId: APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
---
+++ AppVuelos/src/types/env.d.tsx
@@ -0,0 +1,10 @@
+declare module '@env' {
+  export const WEBCLIENT_ID: string;
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const DATABASE_UR: string;
+  export const PROJECT_ID: string;
+  export const STORAGE_BUCKET: string;
+  export const MESSAGINGSENDER_ID: string;
+  export const APP_ID: string;
+}
