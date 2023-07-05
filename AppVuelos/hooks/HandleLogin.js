f3e6ec44eb7e0dc597d1de6baf2337fb9a30d867
DerekAyala <derek.ayla12345@hotmail.com>
2023-07-04 19:34:28 -0600

    Fixed Sign-Out functionality for email/password users
    - Updated the sign-out logic to handle users who signed in with email and password.
    - Implemented the necessary components and methods to handle the sign-out process for email/password users.
    - Tested the sign-out feature for both Google and email/password users to ensure successful log out.
    
    Co-authored-by: Eduardoespl <eduardoesp3317@gmail.com>

---
+++ AppVuelos/hooks/HandleLogin.js
@@ -0,0 +1,23 @@
+import auth from '@react-native-firebase/auth';
+
+const handleLogIn = async (email, password) => {
+  try {
+    const userCredential = await auth().signInWithEmailAndPassword(
+      email,
+      password,
+    );
+    const user = userCredential.user;
+    console.log('User logged in successfully:', user);
+    return user;
+  } catch (error) {
+    console.log('Log in error', error);
+    switch (error.code) {
+      case 'auth/invalid-email':
+        return {typeError: 'email', message: 'That email address is invalid!'};
+      case 'auth/wrong-password':
+        return {typeError: 'password', message: 'Invalid password'};
+    }
+  }
+};
+
+export default handleLogIn;
